05fe463a8590a72ec39fb18c45b0c6fdb14daa2f
shalithacode <shalithalakruwan1998@gmail.com>
2021-02-18 00:56:43 +0530

    Added references

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+//score references
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+const current0 = document.getElementById("current--0");
+const current1 = document.getElementById("current--1");
+
+//image reference
+const diceImg = document.querySelector(".dice");
+
+//button references
+const rollBtn = document.querySelector('.btn--roll');
+const holdBtn = document.querySelector('.btn--hold');
+const newBtn = document.querySelector('.btn--new');
+
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+
